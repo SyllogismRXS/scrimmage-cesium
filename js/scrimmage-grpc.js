9c33b28469262f899cb9f3bfef85469026ab5076
Kevin DeMarco <kevin.demarco@gtri.gatech.edu>
2019-07-18 18:39:26 -0400

    working from scrimmage to cesium

--- js/scrimmage-grpc.js
+++ js/scrimmage-grpc.js
@@ -1,103 +1,104 @@
-module.exports = function(forward_socket) {
-    var module = {};
+'use strict';
 
-    ///////////////////////////////
-    // Load GRPC
-    var PROTO_PATH = __dirname + '/../scrimmage/src/proto';
-    var grpc = require('grpc');
-    var protoLoader = require('@grpc/proto-loader');
+var Cesium = require("cesium");
 
-    // Suggested options for similarity to existing grpc.load behavior
-    var packageDefinition = protoLoader.loadSync(
-        PROTO_PATH + '/scrimmage/proto/Scrimmage.proto',
-        {keepCase: true,
-         longs: String,
-         enums: String,
-         defaults: true,
-         oneofs: true,
-         includeDirs: [PROTO_PATH]
-        });
-    var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
-    // The protoDescriptor object has the full package hierarchy
-    var scrimmage_proto = protoDescriptor.scrimmage_proto
+module.exports = class ScrimmageGRPC {
+    constructor(socket) {
+        this.socket = socket;
 
-    function send_reply(callback, status) {
-        var reply = { success: status };
-        callback(null, reply);
+        ///////////////////////////////
+        // Load GRPC
+        var PROTO_PATH = __dirname + '/../scrimmage/src/proto';
+        var grpc = require('grpc');
+        var protoLoader = require('@grpc/proto-loader');
+
+        // Suggested options for similarity to existing grpc.load behavior
+        var packageDefinition = protoLoader.loadSync(
+            PROTO_PATH + '/scrimmage/proto/Scrimmage.proto',
+            {keepCase: true,
+             longs: String,
+             enums: String,
+             defaults: true,
+             oneofs: true,
+             includeDirs: [PROTO_PATH]
+            });
+        var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+        // The protoDescriptor object has the full package hierarchy
+        var scrimmage_proto = protoDescriptor.scrimmage_proto
+
+        function send_reply(callback, status) {
+            var reply = { success: status };
+            callback(null, reply);
+        }
+
+        var that = this;
+        function SendFrame(call, callback) {
+            // lon, lat, height
+            let origin = new Cesium.Cartesian3.fromDegrees(-120.767925, 35.721025, 300.0);
+
+            var ENU = new Cesium.Matrix4();
+            Cesium.Transforms.eastNorthUpToFixedFrame(origin, Cesium.Ellipsoid.WGS84, ENU);
+
+            for (let i = 0; i < call.request.contact.length; i++) {
+                let cnt = call.request.contact[i];
+                let position = new Cesium.Cartesian3(cnt.state.position.x,
+                                                     cnt.state.position.y,
+                                                     cnt.state.position.z);
+                var finalPos = Cesium.Matrix4.multiplyByPoint(ENU, position, new Cesium.Cartesian3());
+
+                call.request.contact[i].state.position.x = finalPos.x;
+                call.request.contact[i].state.position.y = finalPos.y;
+                call.request.contact[i].state.position.z = finalPos.z;
+            }
+
+            that.socket.emit('frame', call.request);
+            send_reply(callback, 1);
+        }
+
+        function SendUTMTerrain(call, callback) {
+            send_reply(callback, 1);
+        }
+
+        function SendSimInfo(call, callback) {
+            send_reply(callback, 1);
+        }
+
+        function SendContactVisual(call, callback) {
+            send_reply(callback, 1);
+        }
+
+        function SendShapes(call, callback) {
+            send_reply(callback, 1);
+        }
+
+        function Ready(call, callback) {
+            send_reply(callback, 1);
+        }
+
+        function SendGUIMsg(call, callback) {
+            send_reply(callback, 1);
+        }
+
+        function SendWorldPointClicked(call, callback) {
+            send_reply(callback, 1);
+        }
+
+        function getGRPCServer() {
+            var server = new grpc.Server();
+            server.addService(scrimmage_proto.ScrimmageService.service, {
+                SendFrame: SendFrame,
+                SendSimInfo: SendSimInfo,
+                SendContactVisual: SendContactVisual,
+                SendShapes: SendShapes,
+                Ready: Ready,
+                SendGUIMsg: SendGUIMsg,
+                SendWorldPointClicked: SendWorldPointClicked,
+                SendUTMTerrain: SendUTMTerrain
+            });
+            return server;
+        }
+        var scrimmage_server = getGRPCServer();
+        scrimmage_server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
+        scrimmage_server.start();
     }
-
-    function SendFrame(call, callback) {
-        // console.log(call.request.contact.length);
-        //for (var i = 0; i < call.request.contact.length; i++) {
-        //    //console.log('Contact ID: %d', call.request.contact[i].id.id);
-        //    console.log(JSON.stringify(call.request.contact[i]));
-        //}
-        frame = JSON.stringify(call.request);
-
-        function cb_closure(socket) {
-            socket.emit('frame',
-        };
-
-        // if (forward_socket == null) {
-        //     console.log('bad socket');
-        // } else {
-        //     forward_socket.emit('frame', frame);
-        // }
-
-        //if
-        //module.socket.emit('frame', frame);
-
-        send_reply(callback, 1);
-    }
-
-    function SendUTMTerrain(call, callback) {
-        send_reply(callback, 1);
-    }
-
-    function SendSimInfo(call, callback) {
-        send_reply(callback, 1);
-    }
-
-    function SendContactVisual(call, callback) {
-        send_reply(callback, 1);
-    }
-
-    function SendShapes(call, callback) {
-        send_reply(callback, 1);
-    }
-
-    function Ready(call, callback) {
-        send_reply(callback, 1);
-    }
-
-    function SendGUIMsg(call, callback) {
-        send_reply(callback, 1);
-    }
-
-    function SendWorldPointClicked(call, callback) {
-        send_reply(callback, 1);
-    }
-
-    function getGRPCServer() {
-        var server = new grpc.Server();
-        server.addService(scrimmage_proto.ScrimmageService.service, {
-            SendFrame: SendFrame,
-            SendSimInfo: SendSimInfo,
-            SendContactVisual: SendContactVisual,
-            SendShapes: SendShapes,
-            Ready: Ready,
-            SendGUIMsg: SendGUIMsg,
-            SendWorldPointClicked: SendWorldPointClicked,
-            SendUTMTerrain: SendUTMTerrain
-        });
-        return server;
-    }
-    var scrimmage_server = getGRPCServer();
-    scrimmage_server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
-    scrimmage_server.start();
-
-    // Create GRPC client back to scrimmage:
-    // new scrimmage_proto.ScrimmageService('localhost:50052', grpc.credentials.createInsecure());
-
-    return module;
 };
--- public/js/socket.js
+++ public/js/socket.js
@@ -1,5 +1,77 @@
+'use strict';
+
 var socket = io.connect('http://localhost:8000');
 
-socket.on('frame', function (data) {
-    console.log('got a frame');
+function transform_state(state) {
+    var heading = Cesium.Math.toRadians(45.0);
+    var pitch = Cesium.Math.toRadians(15.0);
+    var roll = Cesium.Math.toRadians(0.0);
+
+    var position = new Cesium.Cartesian3(state.position.x, state.position.y, state.position.z);
+    var orientation = new Cesium.Transforms.headingPitchRollQuaternion(position, new Cesium.HeadingPitchRoll(heading, pitch, roll));
+
+    return {
+        position: position,
+        orientation: orientation,
+    };
+}
+
+function update_entity(contact, entity) {
+    var heading = Cesium.Math.toRadians(45.0);
+    var pitch = Cesium.Math.toRadians(15.0);
+    var roll = Cesium.Math.toRadians(0.0);
+
+    let state = transform_state(contact.state);
+    entity.position = state.position;
+    entity.orientation = entity.orientation;
+}
+
+function add_entity(contact, id_str) {
+
+    let state = transform_state(contact.state);
+
+    //Actually create the entity
+    var entity = viewer.entities.add({
+
+        id: id_str,
+
+        // //Set the entity availability to the same interval as the simulation time.
+        // availability : new Cesium.TimeIntervalCollection([new Cesium.TimeInterval({
+        //     start : start,
+        //     stop : stop
+        // })]),
+
+        //Use our computed positions
+        position : state.position,
+
+        //Automatically compute orientation based on position movement.
+        orientation : state.orientation,
+
+        //Load the Cesium plane model to represent the entity
+        model : {
+            uri : '../models/CesiumAir/Cesium_Air.gltf',
+            minimumPixelSize : 64
+        },
+    });
+}
+
+socket.on('frame', function (frame) {
+    // Suspend events during batch entity updates for performance
+    viewer.entities.suspendEvents();
+
+    for (let i = 0; i < frame.contact.length; i++) {
+        let id_str = frame.contact[i].id.id.toString();
+        let entity = viewer.entities.getById(id_str)
+        if (entity != undefined) {
+            update_entity(frame.contact[i], entity);
+        } else {
+            console.log('add entity');
+            add_entity(frame.contact[i], id_str);
+        }
+    }
+    // Resume events after the batch entity update
+    viewer.entities.resumeEvents();
 });
+
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiYmQ0NmZiZC0zZGNlLTQ4ZWMtODJiYi0xYTY2OTk4MDMxNDkiLCJpZCI6ODk1MCwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU1MzExNjM4M30.cUiEWi_tRRvqkGIY7rNG40aVA9rPKaz8vYF2wQuaLyU';
+var viewer = new Cesium.Viewer('cesiumContainer');
